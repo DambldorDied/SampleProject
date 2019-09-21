0f16d89722f3d09c83c9fe52a9db1d002750563c
Тимур Исмагилов <tim.ismagilov2013@yandex.ru>
2019-09-21 13:31:03 +0300

    fufufu1 was remastered  all types JavaJava

---
+++ src/main/java/fubyaka1/Main.java
@@ -0,0 +1,16 @@
+package fubyaka1;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Alloha world");
+        byte t = 2; //1 b
+        short i = 11; //2 b
+        int s = 4444; //4 b
+        long tu = 8888888884234324L;// 8 b
+        float u = 4.6f;// 4 b
+        double p = 7777777775.4415435324513453414535241453; // 8 b
+        char id = 'i'; // 2 b
+        boolean d = false; // 1 - 4 b
+    }
+}
